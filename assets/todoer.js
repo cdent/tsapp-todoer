eda1165f0524df1a6b505f75ddcdd42d88597ab2
Chris Dent <chris.dent@gmail.com>
2012-08-01 16:17:06 +0100

    README and jslint cleanups

--- assets/todoer.js
+++ assets/todoer.js
@@ -1,12 +1,10 @@
+/*jslint browser: true */
 $(function() {
     "use strict";
 
-    // see https://gist.github.com/1200559/1c2b2093a661c4727958ff232cd12de8b8fb9db9
-    var adler32 = function(a){for(var b=65521,c=1,d=0,e=0,f;f=a.charCodeAt(e++);d=(d+c)%b)c=(c+f)%b;return(d<<16)|c};
-
     function addTodo(tiddler) {
-        var item = $('<li>').attr({'data-tiddler': tiddler.title})
-            .append('<label><input type="checkbox"><p>' + tiddler.text + 
+        var item = $('<li>').attr({'data-tiddler': tiddler.title}).
+            append('<label><input type="checkbox"><p>' + tiddler.text +
                 '</p></label>');
         $('.todos').append(item);
     }
@@ -26,16 +24,17 @@ $(function() {
         var textarea = $(this).find('textarea'),
             tagsarea = $(this).find('input[name="tags"]'),
             text = textarea.val(),
-            tags = tagsarea.val().split('/\s+/'),
+            tags = tagsarea.val().split(/\s+/),
             hash = adler32(text + tags + Date.now()),
             space = tiddlyweb.status.space.name,
             uri = '/bags/' + space + '_public/tiddlers/'
-                + encodeURIComponent(hash);
-    
-        tags.push('todo:active')
+                + encodeURIComponent(hash),
+            tiddler,
+            tiddlerJSON;
 
-        var tiddler = {text: text, tags: tags};
-        var tiddlerJSON = JSON.stringify(tiddler);
+        tags.push('todo:active');
+        tiddler = {text: text, tags: tags};
+        tiddlerJSON = JSON.stringify(tiddler);
 
         $.ajax({
             url: uri,
@@ -51,4 +50,7 @@ $(function() {
         });
 
     });
+    // see https://gist.github.com/1200559/1c2b2093a661c4727958ff232cd12de8b8fb9db9
+    var adler32 = function(a){for(var b=65521,c=1,d=0,e=0,f;f=a.charCodeAt(e++);d=(d+c)%b)c=(c+f)%b;return(d<<16)|c};
+
 });
